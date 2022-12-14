92a6fe2c14d4905654854104d844934af06cbd3e
Pooja-sisodia <poojasisodia406@gmail.com>
2022-12-14 21:36:42 +0530

    work

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -4,6 +4,30 @@ const valid_url = require('valid-url');
 const mongoose = require('mongoose');
 const axios = require('axios');
 
+const redis = require("redis");
+const { promisify } = require("util");
+
+//1. Connect to the redis server
+const redisClient = redis.createClient(
+  19949,
+  "redis-19949.c301.ap-south-1-1.ec2.cloud.redislabs.com",
+  { no_ready_check: true }
+);
+redisClient.auth("Ai50nD8uOQ40ib96RQLUPQ5YsiCJVGlB", function (err) {
+  if (err) throw err;
+});
+
+redisClient.on("connect", async function () {
+  console.log("Connected to Redis..");
+});
+
+
+//2. Prepare the functions for each command
+
+const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
+const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
+ 
+
 //----------------------------------------------------------------------------------------//
 //                                1. API -  POST/url/shorten
 //---------------------------------------------------------------------------------------//
@@ -19,15 +43,23 @@ const createShortUrl = async function(req, res) {
     if (typeof longUrl === "undefined" || longUrl === null ||
        (typeof longUrl === "string" && longUrl.length === 0)
     ) return res.status(400).send({ status: false, message:"Please enter a valid <longUrl>."});
-
+   // I can use axios here instead of regex to handle the long url
     let regex =/(?:https?):\/\/(\w+:?\w*)?(\S+)(:\d+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/
     if (!regex.test(longUrl)) return res.status(400).send({status:false, message:"please provide valid LongUrl."});
+    
+    const findUrl = await urlModel.findOne({longUrl:longUrl})
+    if(findUrl){ const result = {
+      urlCode: findUrl.urlCode,
+      longUrl: findUrl.longUrl,
+      shortUrl: findUrl.shortUrl
+    };
 
+    return res.status(200).send({ status: true, message: "this Url is already present", data:result});
+  }
       const urlCode = shortid.generate().toLowerCase();
-      const baseUrl = "http://localhost:3000";
-      const shortUrl = baseUrl + "/" + urlCode;
+      const baseUrl = `http://localhost:3000/${urlCode}`;
 
-      const data = { longUrl, shortUrl, urlCode };
+      const data = { longUrl, baseUrl, urlCode };
       const createData = await urlModel.create(data);
     
       const result = {
@@ -66,4 +98,3 @@ const getUrlByParams = async function(req,res){
 }
 
 module.exports = {createShortUrl, getUrlByParams};
-
